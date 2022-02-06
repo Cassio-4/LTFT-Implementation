69296c192e930733dd414d7aab6f89b1025be823
Cassio-4 <cassiobertasinascimento@gmail.com>
2022-02-05 21:13:06 -0300

    leaving config.py ready for colab execution

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 arcface_config = {
     "model_path": "./resnet18_110.pth",
-    "mode": "cpu",
+    "mode": "cuda",
     "threshold": 0.8,
     "batch_size": 10
 }
 _3ddfa_config = {
-    "mode": "cpu",
+    "mode": "cuda",
     "bbox_init": "one",
     "path_3ddfa_model": "./3DDFA/models/phase1_wpdc_vdc.pth.tar",
     "recognition_threshold": 0.7
@@ -18,13 +18,13 @@ config_dict = {
     "arcface_config": arcface_config,
     "3ddfa_config": _3ddfa_config,
     "data_association_config": data_association_config,
-    "show": True,
-    "write_txt": False,
-    "videos_folder": "/home/cassio/CrowdedDataset/VideoDataset/",
+    "show": False,
+    "write_txt": True,
+    "videos_folder": "./data/VideoDataset/",
     "videos": ["MOT17-09_video.avi", "MOT17-01_video.avi", "MOT17-04_video.avi", "Street_CutVideo.avi",
                "Sidewalk_CutVideo.avi", "Bengal_CutVideo.avi", "Terminal1_CutVideo.avi", "Terminal2_CutVideo.avi",
                "Terminal3_CutVideo.avi", "Terminal4_CutVideo.avi", "Shibuya_CutVideo.avi", "Choke1_CutVideo.avi",
                "Choke2_CutVideo.avi"],
-    "dets_folder": "/home/cassio/CrowdedDataset/DetectionFiles/Yolov5/yolov5s/Yolov5s-nothresh-clean-scoxyXY"
+    "dets_folder": "./data/detections/"
 }
 
